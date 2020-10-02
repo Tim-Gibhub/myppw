0d960360cfc6b723f76df40719495d4a673d8373
Jiyuan Huang <huangpifu2019@gmail.com>
2020-10-02 03:02:00 -0400

    xxxx

---
+++ public/Scripts/app.js
@@ -0,0 +1,39 @@
+// IIFE
+(function(){
+    function Start()
+    {
+        console.log("App Started..");
+        if(document.title=="Contact")
+        {
+            let sendButton=document.getElementById("sendButton");
+            let cancelButton=document.getElementById("cancelButton");
+            let form=document.forms[0];
+
+            sendButton.addEventListener("click",(event) =>{
+                event.preventDefault();
+                let fullName=document.getElementById("fullName").value;
+                let contactNumber=document.getElementById("contactNumber").value;
+                let emailAddress=document.getElementById("emailAddress").value;
+                let message=document.getElementById("message").value;
+                //console.info("hhhhh");
+                console.info(`full Name: ${fullName}
+                Contact Number: ${contactNumber}
+                Email Address : ${emailAddress}
+                message       : ${message}
+                `)
+                form.reset();
+            })
+
+
+            cancelButton.addEventListener("click",(event) =>{
+                event.preventDefault();
+                if(confirm("Are you Sure?"))
+                {
+                    location.href="/";
+                }
+            })
+
+        }
+    }
+    window.addEventListener("load",Start);
+})();
---
+++ routes/index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'Personal-Mission-Statement',
+    S1:'I believe by knowing what I value, I truly know what I want.' ,
+    l1:'active',
+    l2: '',
+    l3: '',
+    l4: '',
+    l5: ''
+  });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { 
+    title: 'About Me',
+    S1:'',
+    l1: '',
+    l2: 'active',
+    l3: '',
+    l4: '',
+    l5: ''  });
+});
+/* GET Products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { 
+    title: 'Products', 
+    S1:'',
+    l1: '',
+    l2: '',
+    l3: 'active',
+    l4: '',
+    l5: ''   });
+});
+/* GET  services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { 
+    title: 'Services',
+    S1:'',
+    l1: '',
+    l2: '',
+    l3: '',
+    l4: 'active',
+    l5: ''  });
+});
+/* GET  Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { 
+    title: 'Contact',
+    S1:'',
+    l1: '',
+    l2: '',
+    l3: '',
+    l4: '',
+    l5: 'active'  });
+});
+module.exports = router;
